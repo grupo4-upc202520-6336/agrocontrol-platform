fa178f3f5b641be1c3ac7673d284d30f78292b2a
HOLGUINUPC <u202220250@upc.edu.pe>
2025-12-05 10:23:33 -0500

    Modify serverBasePath in development environment
    Updated serverBasePath to remove API version.

--- agrocontrol-frontend/src/environments/environment.development.ts
+++ agrocontrol-frontend/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   //production: false,
   //serverBasePath: "https://back-end-3-2tfq.onrender.com/api/v1"
   production: false,
-  serverBasePath: "http://20.63.25.51:8080/api/v1"
+  serverBasePath: "http://20.63.25.51:8080"
 };
