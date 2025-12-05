d29f3a9af38dba1d2959f9440f6f913641ee4f66
HOLGUINUPC <u202220250@upc.edu.pe>
2025-12-05 03:09:31 -0500

    Corrigiendo encoding en todos los properties

--- agrocontrol-frontend/src/environments/environment.ts
+++ agrocontrol-frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   //production: true,
   //serverBasePath: "https://back-end-3-2tfq.onrender.com/api/v1"
   production: true,
-  serverBasePath: "http://localhost:8080/api"
+  serverBasePath: "http://localhost:8080"
 };
