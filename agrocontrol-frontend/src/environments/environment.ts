c2c58ef1c7d02a66ee5adbbcb6d799ed0f89f807
HOLGUINUPC <u202220250@upc.edu.pe>
2025-12-05 03:34:55 -0500

    Clean properties encoding for all remaining services

--- agrocontrol-frontend/src/environments/environment.ts
+++ agrocontrol-frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   //production: true,
   //serverBasePath: "https://back-end-3-2tfq.onrender.com/api/v1"
   production: true,
-  serverBasePath: "http://20.63.25.51:8080/api/v1"
+  serverBasePath: "http://20.63.25.51:8080"
 };
